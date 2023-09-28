a41fd86ac8c4d6d60a3421a03ec410e200f1a976
Akash Puri <104100720+akki9689@users.noreply.github.com>
2023-09-29 00:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
+const articleRegex = /^(?:the|an|a)\s+/i;
+
+  // Sort the array while excluding articles and ignoring case
+  names.sort(function(a, b) {
+    const nameA = a.replace(articleRegex, '').toLowerCase();
+    const nameB = b.replace(articleRegex, '').toLowerCase();
+    return nameA.localeCompare(nameB);
+  });
+sortBandNames(bandNames);
+
+// Get the <ul> element with the id 'band'
+const ulElement = document.getElementById('band');
+
+// Create and append <li> elements for each sorted name
+bandNames.forEach(function(name) {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});
+In this code, we first define a sortBandNames function that sorts the array while excluding articles and ignoring case. Then, we apply this function to the bandNames array, sort it, and create <li> elements for each name, appending them to the <ul> element with the id 'band'.
+
+
+
+
+
 
